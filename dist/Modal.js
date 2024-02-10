6a559ebdcaa12ace8b93bfa7508b78c045e57f7a
stoyb <soniatoyb@yahoo.fr>
2024-02-10 20:06:09 +0100

    build new Modal Component

--- dist/Modal.js
+++ dist/Modal.js
@@ -5,7 +5,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.Modal = Modal;
 var _react = _interopRequireDefault(require("react"));
-var _ModalModule = _interopRequireDefault(require("./Modal.module.css"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function Modal(_ref) {
   let {
@@ -13,15 +12,47 @@ function Modal(_ref) {
     handleClose,
     children
   } = _ref;
+  const modalContainerBlock = {
+    display: 'block',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    position: 'fixed',
+    left: 0,
+    top: 0,
+    width: '100%',
+    height: '100%',
+    overflow: 'auto'
+  };
+  const modalContainerNone = {
+    display: 'block'
+  };
+  const modalContent = {
+    position: 'relative',
+    backgroundColor: '#ffffff',
+    margin: '300px auto',
+    padding: '20px',
+    width: '510px',
+    borderRadius: '8px',
+    boxShadow: '0 10px 20px 0 rgba(0, 0, 0, 0.3)',
+    display: 'flex'
+  };
+  const modalText = {
+    width: '146px'
+  };
+  const modalButton = {
+    position: 'relative',
+    top: '-32px',
+    left: '362px',
+    fontSize: '42px'
+  };
   return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement("div", {
-    className: isOpen ? _ModalModule.default.modalContainerBlock : _ModalModule.default.modalContainerNone
+    style: isOpen ? modalContainerBlock : modalContainerNone
   }, /*#__PURE__*/_react.default.createElement("div", {
-    className: _ModalModule.default.modalContent
+    style: modalContent
   }, /*#__PURE__*/_react.default.createElement("p", {
-    className: _ModalModule.default.modalText
+    style: modalText
   }, children), /*#__PURE__*/_react.default.createElement("button", {
     onClick: handleClose,
-    className: _ModalModule.default.modalButton
+    style: modalButton
   }, "X"))));
 }
 ;
--- src/lib/Modal.jsx
+++ src/lib/Modal.jsx
@@ -1,14 +1,50 @@
 import React from 'react';
-import styles from './Modal.module.css';
 
 export function Modal({ isOpen, handleClose, children }) {
   
+  const modalContainerBlock = {
+    display: 'block',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    position: 'fixed',
+    left: 0,
+    top: 0,
+    width: '100%',
+    height: '100%',
+    overflow: 'auto'
+  };
+
+  const modalContainerNone = {
+    display: 'block'
+  }
+
+  const modalContent = {
+    position: 'relative',
+    backgroundColor: '#ffffff',
+    margin: '300px auto',
+    padding: '20px',
+    width: '510px',
+    borderRadius: '8px',
+    boxShadow: '0 10px 20px 0 rgba(0, 0, 0, 0.3)',
+    display: 'flex'
+  }
+
+  const modalText = {
+    width: '146px'
+  }
+
+  const modalButton = {
+    position:'relative',
+    top: '-32px',
+    left: '362px',
+    fontSize: '42px'
+} 
+  
   return (
     <>
-    <div className={isOpen ? styles.modalContainerBlock : styles.modalContainerNone}>
-      <div className={styles.modalContent}>
-        <p className={styles.modalText}>{children}</p>
-        <button onClick={handleClose} className={styles.modalButton}>X</button>
+    <div style={isOpen ? modalContainerBlock : modalContainerNone}>
+      <div style={modalContent}>
+        <p style={modalText}>{children}</p>
+        <button onClick={handleClose} style={modalButton}>X</button>
       </div>
     </div>
     </>
